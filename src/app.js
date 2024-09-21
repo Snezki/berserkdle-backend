41584409ad573469f87208300ec7bc82fba01a57
Snezki <romain.menard2018@campus-eni.fr>
2024-09-22 01:05:23 +0200

    setup postgres

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const morgan = require('morgan')
 require('dotenv').config()
 
+const pool = require('./config/db');
+
 const app = express()
 
 app.use(cors())
@@ -12,5 +14,15 @@ app.use(morgan('dev'))
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Berserkdle API' })
 });
+
+app.get('/api/test-db', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT NOW()');
+      res.json({ message: 'Database connected successfully', time: result.rows[0] });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Database connection error' });
+    }
+  });
 
 module.exports = app;
